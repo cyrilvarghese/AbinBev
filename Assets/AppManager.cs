5125a15e952e1a085c0612f6c9f1d138630e3c81
cyrilvarghese <cyrilpdev@gmail.com>
2019-05-05 13:11:51 +0530

    first commit

---
+++ Assets/AppManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            UnityEngine.XR.XRSettings.eyeTextureResolutionScale = 1.50f;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Easy Input for Gear VR/Scripts/Miscellaneous/ChangeColor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using EasyInputVR.Core;
+
+namespace EasyInputVR.Misc
+{
+
+
+    public class ChangeColor : MonoBehaviour
+    {
+
+        MeshRenderer myRenderer;
+        Material myMaterial;
+        Color originalColor;
+        public GameObject deac;
+
+        void Start()
+        {
+            myRenderer = this.GetComponent<MeshRenderer>();
+            myMaterial = myRenderer.material;
+            originalColor = myMaterial.color;
+        }
+
+        public void changeColor(ButtonClick button)
+        {
+            myMaterial.color = Color.green;
+       
+        }
+
+        public void changeColorBack(ButtonClick button)
+        {
+            myMaterial.color = originalColor;
+        }
+
+        public void toggleColor()
+        {
+            
+            if (myMaterial.color == originalColor)
+            {
+                myMaterial.color = Color.green;
+                if (deac)
+                    deac.gameObject.GetComponent<Animator>().enabled = false;
+            }
+                
+            else
+            {
+                myMaterial.color = originalColor;
+              //  if (deac)
+                   // deac.gameObject.GetComponent<Animator>().enabled = true;
+            }
+               
+        }
+    }
+}
