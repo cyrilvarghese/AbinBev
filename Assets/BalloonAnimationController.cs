bc5e3ae6da60cc393e5ae47bcbf424a25a3494aa
cyrilvarghese <cyrilpdev@gmail.com>
2019-08-22 23:06:06 +0530

    new scene added

---
+++ Assets/BalloonAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonAnimationController : MonoBehaviour {
+    public Animator SunroofAnimator;
+	// Use this for initialization
+	void Start () {
+      
+
+    }
+    private void OnEnable()
+    {
+         SunroofAnimator.GetComponent<Animator>().Play("balloon-up"); 
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            
+        }
+		
+	}
+}
---
+++ Assets/TransferCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransferCamera : MonoBehaviour {
+    public GameObject NextCameraParent;
+    public GameObject CameraContainer;
+    public bool isTransferOnEnable;
+    public bool localRectComponent;
+    // Use this for initialization
+    void OnEnable() {
+        if(isTransferOnEnable)
+        CameraContainer.transform.SetParent(NextCameraParent.transform, false);
+    }
+    public void TransferCameraToNextParent()
+    {
+        CameraContainer.transform.SetParent(NextCameraParent.transform, localRectComponent);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
